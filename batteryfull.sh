713b8b663caa07dc5f9e4005a6b09731e9663d36
basha <basha502@gmail.com>
2020-10-01 09:41:40 +0530

    battery full and restarting the panel

---
+++ batteryfull.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+while true
+do
+    export DISPLAY=:0.0
+    battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+    
+    if [[ $(acpi -b) =~ "Charging" ]]; then
+        if [ $battery_percent -gt 90 ]; then
+            notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
+        fi
+    fi
+    
+    sleep 300 # (5 minutes)
+done
