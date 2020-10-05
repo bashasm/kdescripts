92affc80c05730f7103a661de564e78e731f6399
basha <basha502@gmail.com>
2020-10-05 22:13:59 +0530

    Update and rename batteryfull.sh to batteryinfo.sh
    Added notifications for Full charge and Low charge

--- batteryinfo.sh
+++ batteryinfo.sh
@@ -9,6 +9,12 @@ do
             notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
         fi
     fi
+
+    if [[ $(acpi -b) =~ "Discharging" ]]; then
+        if [ $battery_percent -lt 20 ]; then
+            notify-send -i "$PWD/batteryfull.png" "Battery almost empty. Please charge." "Level: ${battery_percent}% "
+        fi
+    fi
     
     sleep 300 # (5 minutes)
 done
